08831fb9c44977057d1d862fa801037f5c581245
Marcelo Lino <marcelo.lino128@hotmail.com>
2021-06-30 10:53:50 -0300

    Primeiro commit

---
+++ src/screens/SignIn/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text}from 'react-native'
+
+export function SingIn(){
+    return (
+        <View>
+            <Text>
+                SingIn Hello Word!
+            </Text>
+        </View>
+    )
+}
